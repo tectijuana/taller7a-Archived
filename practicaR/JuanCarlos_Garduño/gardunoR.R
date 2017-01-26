c2362c13c48f7539e4b7f9e451279664b7deb2ae
Juan Carlos Garduño Muñoz <isc.juancarlos.garduno@gmail.com>
2017-01-25 17:27:50 -0800

    update

--- practicaR/JuanCarlos_Garduño/gardunoR.R
+++ practicaR/JuanCarlos_Garduño/gardunoR.R
@@ -1,11 +1,54 @@
-list.files()
-read.csv("gmData.csv")
-datos <- read.csv("gmData.csv")
-print(datos)
-datos <- data.frame(Funds, Department)
-datos$Funds
-datos$Department
-plot(datos$Funds, datos$Department)
-install.packages("ggplot2")
-library(ggplot2)
-qplot(datos$Funds, datos$Department)
+# *------------------------------------------------------------------
+# | PROGRAM NAME: gardunoR.R
+# | DATE: 19/01/2017
+# | CREATED BY: Juan Carlos Garduño Muñoz  
+# | PROJECT FILE: practicaR              
+# *----------------------------------------------------------------
+# | PURPOSE: introducción al lenguaje R realizando gráficas           
+# |
+# *------------------------------------------------------------------
+# | COMMENTS:               
+# |
+# |  1: Se desarrollan dos tipos de gráficas 
+# |  2: Una B.D de 1000 registros.
+# |  3: 
+# |*------------------------------------------------------------------
+# | DATA USED:  gmData.csv             
+# |
+# |
+# |*------------------------------------------------------------------
+# | CONTENTS:               
+# |
+# |  PART 1:  
+# |  PART 2: 
+# |  PART 3: 
+# *-----------------------------------------------------------------
+# | UPDATES:               
+# |
+# |
+# *------------------------------------------------------------------
+
+	list.files()
+	read.csv("gmData.csv")
+	datos <- read.csv("gmData.csv")
+	print(datos)
+	datos <- data.frame(Funds, Department)
+	datos$Funds
+	datos$Department
+		plot(datos$Funds, datos$Department)
+
+	install.packages("ggplot2")
+	library(ggplot2)
+		qplot(datos$Funds, datos$Department)
+
+# *------------------------------------------------------------------
+# |                
+# |
+# |   
+# |
+# |                
+# *-----------------------------------------------------------------
+
+# *------------------------------------------------------------------
+# |                       
+# *-----------------------------------------------------------------
