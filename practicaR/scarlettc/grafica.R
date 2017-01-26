089adc3474d45cd50c4c28ae852d8a5cdb2789af
scarlettc <scarlett.oropeza@gmail.com>
2017-01-25 17:32:54 -0800

    Update

--- practicaR/scarlettc/grafica.R
+++ practicaR/scarlettc/grafica.R
@@ -2,14 +2,18 @@
 # | PROGRAM NAME:R CHART
 # | DATE: 19/01/17
 # | CREATED BY: SCARLETT ITZEL OROPEZA CRUZ
+# | PROJECT FILE: grafica.R
 # *------------------------------------------------------------------
 # | PURPOSE: TEST AND PRACTICE CHARTS IN R LANGUAGE
-# |
+# *------------------------------------------------------------------
+# | DATA USED: 1,000 ROWS OF DATA MADE IT BY MOCKAROO
 # *------------------------------------------------------------------
 
 # IMPORT CSV FILE
 data1 <- read.csv(file="data.csv",head=TRUE, sep=",")
 data1
+
+#MAKE PLOT CHARTS
 plot(data1)
 plot(data1$ID,data1$Nombre)
 plot(data1$Nombre)
@@ -17,4 +21,3 @@ plot(data1)
 plot(data1$Apellido)
 plot(data1$ID)
 plot(data1)
-save.image("C:\\Users\\itzel\\Desktop\\GraficaR.RData")
