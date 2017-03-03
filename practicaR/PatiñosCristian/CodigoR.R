51e9809cb196511ed70423e352947cdea60afd06
crispatino <cristianpatino3@gmail.com>
2017-03-02 17:45:10 -0800

    PracticaR

---
+++ practicaR/PatiñosCristian/CodigoR.R
@@ -0,0 +1,34 @@
+# *------------------------------------------------------------------
+# | PROGRAM NAME: Plotear en R
+# | DATE: 25/1/2017
+# | CREATED BY: Patiño Peinado Cristian Antonio
+# | PROJECT FILE: plotear.csv             
+# *----------------------------------------------------------------
+# | PURPOSE:  aprender lenguaje R y el uso de Git          
+# |
+# *------------------------------------------------------------------
+# | COMMENTS:               
+# |
+# |  1: 1000 registros en archivo csv 
+# |  2: Graficado en R (R Languaje)
+# |  3: 
+# |*------------------------------------------------------------------
+
+
+atuador <- read.csv("plotear.csv")
+gdp <- read.table("plotear.csv", header=TRUE)
+plot (gdp)
+
+
+
+# *------------------------------------------------------------------
+# |  el codigo es corto pero realiza la funcion de leer el archivo y               
+# |  graficarlo.
+# |   
+# |
+# |                
+# *-----------------------------------------------------------------
+
+# *------------------------------------------------------------------
+# |                       
+# *-----------------------------------------------------------------
